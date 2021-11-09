7332f602520ec3efae7490316fa89562122b8c60
yaninaslab <yana.belash@gmail.com>
2021-11-08 22:51:20 -0500

    Added comments to the code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-
+// Presenting an array of strings - 10 book names
 var books = ["Broken Glass", "Harry Potter and the Goblet of Fire", "A Little Life", "The Tipping Point", "Darkmans",
-"The Siege", "Light", "Visitation", "Bad Blood", "Noughts & Crosses"]
-
+"The Siege", "Light", "Visitation", "Bad Blood", "Noughts & Crosses"];
+// Printing every book's name in the order they appear in the array
 for(var i=0; i < books.length; i++) {
     console.log(books[i])
 }
-
+// Creating an array of JavaScript Objects with specified key-value pairs
 var tweets = [{
     tweet: "This is the first tweet",
     username: "john",
@@ -25,6 +25,7 @@ var tweets = [{
     age: "25",
 }
 ]
+// Printing out the specific values based on the keys mentioned
 for(var i=0; i < tweets.length; i++) {
     console.log(tweets[i]['username']);
     console.log(tweets[i]['created_at']);
